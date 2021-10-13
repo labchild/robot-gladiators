809a2b78314d0cb853cb6dfae0c9c9467140c75c
l bates childs <labchilds@gmail.com>
2021-10-13 16:13:01 -0400

    html outlined and js added

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+console.log("Welcome, Robots!");
+
+// "fight" function declared
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+// fight();
