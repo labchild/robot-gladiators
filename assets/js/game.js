6ee19fa4a3a4cd855657f1148e2f41216ffd1850
l bates childs <labchilds@gmail.com>
2021-10-20 17:09:48 -0400

    add highscore storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -168,10 +168,25 @@ var endGame = function () {
 
     //if player is still alive, player win
     if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+        window.alert("Great job, you've survived the battle! You now have a score of " + playerInfo.money + ".");
     } else {
         window.alert("You've lost your robot in battle.");
     }
+
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money +"!");
+    } else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+    
     //ask if player wants to go again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
